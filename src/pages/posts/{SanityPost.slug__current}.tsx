4201c2e239c363090ccd9205412a3d7fe6ca7177
Marumbo Sichinga <marumbok@gmail.com>
2022-05-06 23:39:50 +0200

    typescript and prettier edits

--- src/pages/posts/{SanityPost.slug__current}.tsx
+++ src/pages/posts/{SanityPost.slug__current}.tsx
@@ -1,48 +1,78 @@
-import PortableText from 'react-portable-text'
-import { graphql } from 'gatsby'
-import { GatsbyImage } from 'gatsby-plugin-image'
-import React from 'react'
-import { Container, Layout } from '../../components/layouts'
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
+import PortableText from 'react-portable-text';
+import { graphql } from 'gatsby';
+import { GatsbyImage } from 'gatsby-plugin-image';
+import React from 'react';
 
+import { Container, Layout } from '../../components/layouts';
 
-export default function Post(props: { data: GatsbyTypes.SanityPostQueryQuery }) {
+export default function Post(props: {
+  data: GatsbyTypes.SanityPostQueryQuery;
+}): JSX.Element {
   return (
     <Layout>
       <Container>
         <Container>
           <Container>
-            <GatsbyImage objectPosition="center top" alt={props.data.sanityPost?.title ?? "Post Image"} className="w-full aspect-w-16 aspect-h-9 lg:aspect-h-7" image={props.data.sanityPost!.mainImage!.asset!.gatsbyImageData!} />
-            <h1 className="mt-8 text-3xl font-bold">{props.data.sanityPost?.title}</h1>
+            <GatsbyImage
+              objectPosition="center top"
+              alt={props.data.sanityPost?.title ?? 'Post Image'}
+              className="w-full aspect-w-16 aspect-h-9 lg:aspect-h-7"
+              image={props.data.sanityPost!.mainImage!.asset!.gatsbyImageData!}
+            />
+            <h1 className="mt-8 text-3xl font-bold">
+              {props.data.sanityPost?.title}
+            </h1>
             <div className="flex items-center justify-between py-3 my-4 mb-8 border-t border-b border-gray-300">
               <div className="flex items-center gap-x-2">
                 <div className="avatar">
                   <div className="w-12 h-12 border rounded-full px-auto">
-                    <GatsbyImage alt={props.data.sanityPost?.title ?? "Post Image"} className="w-12 h-12" image={props.data.sanityPost!.mainImage!.asset!.gatsbyImageData!} />
+                    <GatsbyImage
+                      alt={props.data.sanityPost?.title ?? 'Post Image'}
+                      className="w-12 h-12"
+                      image={
+                        props.data.sanityPost!.mainImage!.asset!
+                          .gatsbyImageData!
+                      }
+                    />
                   </div>
                 </div>
-                <p>Written by <strong>{props.data.sanityPost?.author?.name}</strong></p>
+                <p>
+                  Written by{' '}
+                  <strong>{props.data.sanityPost?.author?.name}</strong>
+                </p>
               </div>
               <p>{humanReadableDate(props.data.sanityPost?.publishedAt!)}</p>
             </div>
-            <PortableText content={props.data.sanityPost?._rawBody!} serializers={{
-              normal: (props) => <p className="my-4" {...props} />,
-            }} />
+            <PortableText
+              content={props.data.sanityPost?._rawBody!}
+              serializers={{
+                normal: (props) => (
+                  <p className="my-8 text-xl leading-8" {...props} />
+                ),
+              }}
+            />
           </Container>
         </Container>
       </Container>
     </Layout>
-  )
+  );
 }
 
-
-export function humanReadableDate(date: string) {
-  const d = new Date(date)
-  return `${d.toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}`
+export function humanReadableDate(date: string): string {
+  const d = new Date(date);
+  return `${d.toLocaleDateString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  })}`;
 }
 
 export const query = graphql`
   query SanityPostQuery($id: String!) {
-    sanityPost(id:  { eq: $id } ) {
+    sanityPost(id: { eq: $id }) {
       title
       publishedAt
       author {
@@ -50,17 +80,17 @@ export const query = graphql`
       }
       _rawBody
       mainImage {
-			asset {
-				url
-				altText
-				gatsbyImageData(
-          width: 1200
-          height: 1000
-					placeholder: DOMINANT_COLOR
-          formats: AUTO
-				)
-			}
-		}
+        asset {
+          url
+          altText
+          gatsbyImageData(
+            width: 1200
+            height: 1000
+            placeholder: DOMINANT_COLOR
+            formats: AUTO
+          )
+        }
+      }
     }
   }
-`
+`;
