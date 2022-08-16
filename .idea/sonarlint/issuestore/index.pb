44db36380e044032ea33f6495e0d91b0ffb980d2
Jeremiah Chienda <jeremiahchienda@gmail.com>
2022-08-16 23:59:56 +0200

    feat: added details to blog page

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,14 @@
+
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+P
+ tests/integration/sample.spec.js,1/e/1e9934d87747b024b581c4717a4ddb924258e5b4
+:
+
+Roadmap.md,4/8/48153c4add77b289d5f731d426a9711c856c8756
+@
+gatsby-config.js,4/f/4f261c6e46142df5e9a7303296b3fca674d719a9
+^
+.src/pages/posts/{SanityPost.slug__current}.tsx,0/4/0418cd90caac679c0efd8cab0bdacae3084eec23
+<
+package.json,7/0/7030d0b2f71b999ff89a343de08c414af32fc93a
--- src/pages/posts/{SanityPost.slug__current}.tsx
+++ src/pages/posts/{SanityPost.slug__current}.tsx
@@ -5,6 +5,7 @@ import { graphql } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 import React from 'react';
 import { ArticleContainer, Layout } from '../../components/layouts';
+import PageTitleComponent from '../../components/views/page-title';
 
 export default function Post(props: {
   data: GatsbyTypes.SanityPostQueryQuery;
@@ -12,16 +13,10 @@ export default function Post(props: {
   return (
     <Layout>
       <ArticleContainer>
-        <GatsbyImage
-          objectPosition="center top"
-          alt={props.data.sanityPost?.title ?? 'Post Image'}
-          className="w-full aspect-w-16 aspect-h-9 lg:aspect-h-10"
-          image={props.data.sanityPost!.mainImage!.asset!.gatsbyImageData!}
+        <PageTitleComponent
+          title={props.data.sanityPost?.title ?? "Some Title"}
         />
-        <h1 className="mt-8 text-3xl font-bold">
-          {props.data.sanityPost?.title}
-        </h1>
-        <div className="flex items-center justify-between py-3 my-4 mb-8 border-t border-b border-gray-300">
+        <div className="flex items-center justify-center py-3 my-2 mb-4 -mt-8 gap-x-4">
           <div className="flex items-center gap-x-2">
             <div className="relative avatar">
               <div className="w-12 h-12 border rounded-full px-auto">
@@ -35,19 +30,31 @@ export default function Post(props: {
               by <strong>{props.data.sanityPost?.author?.name}</strong>
             </p>
           </div>
-          <p>{humanReadableDate(props.data.sanityPost?.publishedAt!)}</p>
+          <p className="opacity-40">{humanReadableDate(props.data.sanityPost?.publishedAt!)}</p>
         </div>
+        <GatsbyImage
+          objectPosition="center top"
+          alt={props.data.sanityPost?.title ?? 'Post Image'}
+          className="w-full rounded aspect-w-16 aspect-h-9 lg:aspect-h-10"
+          image={props.data.sanityPost!.mainImage!.asset!.gatsbyImageData!}
+        />
         <PortableText
           projectId={process.env.GATSBY_SANITY_PROJECT_ID}
           dataset={process.env.GATSBY_SANITY_DATASET}
           content={props.data.sanityPost?._rawBody!}
           imageOptions={{ w: 1600, h: 1024, fit: 'min' }}
           serializers={{
+            h1: ({ children }) => <h1 className="my-2 text-2xl">{children}</h1>,
+            h2: ({ children }) => <h2 className="my-2 text-2xl font-bold">{children}</h2>,
+            h3: ({ children }) => <h3 className="my-2 text-2xl font-semibold">{children}</h3>,
+            h4: ({ children }) => <h3 className="text-2xl">{children}</h3>,
+            ul: ({ children }) => <ul className="my-4">{children}</ul>,
+            li: ({ children }) => <li className="text-lg">&#8594; {children}</li>,
             normal: (props: unknown) => (
-              <p className="my-8 text-xl leading-8" {...props} />
+              <p className="my-6 text-xl leading-8" {...props} />
             ),
             blockquote: (props: unknown) => (
-              <blockquote className="my-8 text-xl" {...props} />
+              <blockquote className="py-4 mx-12 my-8 text-2xl font-light border-t border-b border-gray-200" {...props} />
             ),
           }}
         />
